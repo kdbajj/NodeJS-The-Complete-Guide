061ad6d9c7199d8a2c61abe20514c842419fc384
Karolina Dbaj <k.dbaj@vp.pl>
2022-11-15 13:08:12 +0100

    The Node Lifecycle & Event Loop

--- Understanding the basics/app.js
+++ Understanding the basics/app.js
@@ -4,6 +4,8 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  //The Node Lifecycle & Event Loop
+  process.exit();
 });
 
 server.listen(3000);
