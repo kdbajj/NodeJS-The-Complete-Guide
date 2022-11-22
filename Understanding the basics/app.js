d8e7613b7d459d3544668b7b47834e4f74ba1351
Karolina Dbaj <k.dbaj@vp.pl>
2022-11-22 10:56:14 +0100

    Routing Requests

--- Understanding the basics/app.js
+++ Understanding the basics/app.js
@@ -3,9 +3,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //The Node Lifecycle & Event Loop
+  // console.log(req.url, req.method, req.headers);
   // process.exit();
+  const url = req.url;
+  if (url === "/") {
+    // res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My first page</title></head>");
+    res.write(
+      `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first page</title></head>");
